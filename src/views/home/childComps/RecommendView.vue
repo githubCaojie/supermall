0f20a0b60f7b3d1ae5cd6dac7b360c46109aa072
caojie <15116241711@163.com>
2020-12-28 18:09:42 +0800

    更新导航

---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="recommends">
+    <div v-for="item in recommends" :key="item.index" class="recommends-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'RecommendView',
+    props: {
+      recommends: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .recommends{
+    display: flex;
+    width: 100%;
+    text-align: center;
+    font-size: 12px;
+
+    padding: 10px 0 20px;
+    border-bottom: 9px solid #eee;
+  }
+  .recommends-item{
+    flex: 1;
+  }
+  .recommends-item img{
+    width: 65px;
+    height: 65px;
+    margin-bottom: 10px;
+  }
+</style>
